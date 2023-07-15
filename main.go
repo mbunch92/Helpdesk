c96735df276f0d3d360c3c2670c50790ee5c666c
michaelbunch <michael1134@hotmail.com>
2023-07-15 10:57:21 -0700

    Added Postgres db.

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"os"
 
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	connStr := "user=postgres dbname=Helpdesk password=Illidan12! host=localhost sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\nSuccessfully connected to database!\n")
 }
